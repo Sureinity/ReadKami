2d481ed9af32f57f306e0791267d4f9d9512e2ea
Altos22 <rovicconstantinorollon@gmail.com>
2024-05-24 21:44:43 +0800

    Logo Icon and SplashScreen

---
+++ app/src/main/java/com/example/readkami_beta/Splash_Screen.java
@@ -0,0 +1,26 @@
+package com.example.readkami_beta;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.readkami_beta.Activities.MainActivity;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash_Screen.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
